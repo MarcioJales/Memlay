61dc5e68ca9e50c4efee46b8c4a3acf86894df82
Márcio Jales <marcio.dojcosta@gmail.com>
2018-03-30 16:31:11 -0300

    Tests modified

--- tests/test.c
+++ tests/test.c
@@ -25,8 +25,11 @@ int main(int argc, char** argv)
 	int res;
 	int *t = (int *) malloc(2);
 	res = sum(argc);
-	int *r = (int *) malloc(300000);
-	prod(res, argc);
+	int i;
+	for(i = 0; i < 4; i++) {
+		int *r = (int *) calloc(10000, 8);
+		prod(res, argc);
+	}
 
 	return 0;
 }
